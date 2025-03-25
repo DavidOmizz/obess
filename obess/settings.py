370b9ab8438b20db640f24961d9d1b890b9de3fc
DavidOmizz <davidomisakin4good@gmail.com>
2025-03-25 08:35:55 +0100

    Did some changes

--- obess/settings.py
+++ obess/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'obess.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'techeraa_testdb',
-#         'USER': 'techeraa_admin',
-#         'PASSWORD': 'techera@admindb',
-#         'HOST': '131.153.148.82',  # Use your database host (or remote server)
-#         'PORT': '3306',  # Default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techeraa_testdb',
+        'USER': 'techeraa_admin',
+        'PASSWORD': 'techera@admindb',
+        'HOST': '131.153.148.82',  # Use your database host (or remote server)
+        'PORT': '3306',  # Default PostgreSQL port
+    }
+}
 
 
 # Password validation
